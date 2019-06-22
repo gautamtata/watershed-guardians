19ddbe80dac78ac963644e2adeb4165f3a167059
Gautam Tata <gautamtata@gmail.com>
2019-06-22 03:40:47 -0700

    border to chart container

---
+++ build/precache-manifest.7ee7aadc6ad8b870435668bfaf82baa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4636449fc6aee6445aae9493cf0f8a41",
+    "url": "/watershed-guardians/index.html"
+  },
+  {
+    "revision": "06289592d3b6aa637ae3",
+    "url": "/watershed-guardians/static/css/2.2614ebd1.chunk.css"
+  },
+  {
+    "revision": "783574e8d65555db4fb9",
+    "url": "/watershed-guardians/static/css/main.2b6c9114.chunk.css"
+  },
+  {
+    "revision": "06289592d3b6aa637ae3",
+    "url": "/watershed-guardians/static/js/2.a67ab808.chunk.js"
+  },
+  {
+    "revision": "783574e8d65555db4fb9",
+    "url": "/watershed-guardians/static/js/main.49168d76.chunk.js"
+  },
+  {
+    "revision": "acbc88cdb640a408ab2b",
+    "url": "/watershed-guardians/static/js/runtime~main.226dcd1f.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/watershed-guardians/precache-manifest.a6ac77db887bd3e62992cad468038499.js"
+  "/watershed-guardians/precache-manifest.7ee7aadc6ad8b870435668bfaf82baa7.js"
 );
 
 self.addEventListener('message', (event) => {
