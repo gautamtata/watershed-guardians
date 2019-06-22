cd03dc3c1e5ae2768508b4db9d5872c2cf52930a
Gautam Tata <gautamtata@gmail.com>
2019-06-21 19:29:42 -0700

    variable name changes

--- src/components/TestPane/checkbox.js
+++ src/components/TestPane/checkbox.js
@@ -1,41 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
 class CheckboxLabels extends Component {
-
-  state={
-    checkedB: (this.props.initializeTrue ? true : false)   
-  }
-
-  handleChange = () =>  {
-     this.props.onClick()
-  
-       this.setState({checkedB: !this.state.checkedB})
-
-    
+  state = {
+    checked: this.props.initializeTrue ? true : false,
   };
 
-  render(){
+  handleChange = () => {
+    this.props.onClick();
+
+    this.setState({ checked: !this.state.checked });
+  };
+
+  render() {
     let label = this.props.label;
     return (
       <FormGroup row>
         <FormControlLabel
           control={
             <Checkbox
-              checked={this.state.checkedB}
-              onChange= {this.handleChange}
-              value="checkedB"
+              checked={this.state.checked}
+              onChange={this.handleChange}
+              value="checked"
               color="primary"
             />
           }
           label={label}
-        />     
+        />
       </FormGroup>
     );
   }
-
 }
 
 export default CheckboxLabels;
