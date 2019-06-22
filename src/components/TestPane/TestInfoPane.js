ab949473ef105298ce4162bce88fa0494520c89d
Gautam Tata <gautamtata@gmail.com>
2019-06-21 19:32:15 -0700

    removing unnecessary lines

--- src/components/TestPane/TestInfoPane.js
+++ src/components/TestPane/TestInfoPane.js
@@ -1,30 +1,33 @@
-import React, {Component} from 'react'
-import './testpane.css'
+import React, { Component } from 'react';
+import './testpane.css';
 
 const MESSAGES = {
-	'ph': "The pH of river water is the measure of how acidic or basic the water is on a scale of 0-14. It is a measure of hydrogen ion concentration. U.S. natural water falls between 6.5 and 8.5 on this scale with 7.0 being neutral. The optimum pH for river water is around 7.4. ",
-	'temperature (deg C)': "Temperature impacts the rates of metabolism and growth of aquatic organisms, rate of plants' photosynthesis, solubility of oxygen in river water, and organisms' sensitivity to disease, parasites, and toxic materials. At a higher temperature, plants grow and die faster, leaving behind matter that requires oxygen for decomposition.",
-	'depth (cm)': "The depth of a river is an indicator of many factors including: rain that year,groundwater depletion or an obstruction in the course of the river.",
-	'dissolved oxygen (ppm)': "An adequate supply of dissolved oxygen gas is essential for the survival of aquatic organisms. A deficiency in this area is a sign of an unhealthy river. There are a variety of factors affecting levels of dissolved oxygen. The atmosphere is a major source of dissolved oxygen in river water."
-}
+  ph:
+    'The pH of river water is the measure of how acidic or basic the water is on a scale of 0-14. It is a measure of hydrogen ion concentration. U.S. natural water falls between 6.5 and 8.5 on this scale with 7.0 being neutral. The optimum pH for river water is around 7.4. ',
+  'temperature (deg C)':
+    "Temperature impacts the rates of metabolism and growth of aquatic organisms, rate of plants' photosynthesis, solubility of oxygen in river water, and organisms' sensitivity to disease, parasites, and toxic materials. At a higher temperature, plants grow and die faster, leaving behind matter that requires oxygen for decomposition.",
+  'depth (cm)':
+    'The depth of a river is an indicator of many factors including: rain that year,groundwater depletion or an obstruction in the course of the river.',
+  'dissolved oxygen (ppm)':
+    'An adequate supply of dissolved oxygen gas is essential for the survival of aquatic organisms. A deficiency in this area is a sign of an unhealthy river. There are a variety of factors affecting levels of dissolved oxygen. The atmosphere is a major source of dissolved oxygen in river water.',
+};
 
-// props:
-// - testName
-// - close()
 class TestInfoPane extends Component {
-	render() {
-		console.log(this.props.testName);
-		return (
-			<div className="test-info-pane"> 
-				<h2>{this.props.testPane}</h2>
-				<i className="close-icon fas fa-times fa-2x" onClick={this.props.close}></i>
-				<br />
-				<h1>{this.props.testName.toUpperCase()}</h1>
-				<div className="message">{MESSAGES[this.props.testName]}</div>
-				
-			</div>
-		)
-	}
+  render() {
+    console.log(this.props.testName);
+    return (
+      <div className="test-info-pane">
+        <h2>{this.props.testPane}</h2>
+        <i
+          className="close-icon fas fa-times fa-2x"
+          onClick={this.props.close}
+        ></i>
+        <br />
+        <h1>{this.props.testName.toUpperCase()}</h1>
+        <div className="message">{MESSAGES[this.props.testName]}</div>
+      </div>
+    );
+  }
 }
 
-export default TestInfoPane
+export default TestInfoPane;
--- src/components/TestPane/testpane.js
+++ src/components/TestPane/testpane.js
@@ -1,53 +1,49 @@
-import React, {Component} from 'react'
-import './testpane.css'
-import TestInfoPane from './TestInfoPane.js'
-import CheckboxLabels from './checkbox'
+import React, { Component } from 'react';
+import TestInfoPane from './testInfoPane.js';
+import CheckboxLabels from './checkbox';
+import './testpane.css';
 
-// props:
-// - selectedTests[]
-// - allTests[]
-// - onClickTest(testName)
-// - title
 class TestPane extends Component {
+  state = {
+    testInfoName: null,
+  };
+  close = () => {
+    this.setState({ testInfoName: null });
+  };
 
-	state = {
-		testInfoName: null
-	}
-	close = () => {
-		this.setState({testInfoName: null})
-	}
+  render() {
+    if (this.state.testInfoName) {
+      return (
+        <div className="test-pane">
+          <TestInfoPane testName={this.state.testInfoName} close={this.close} />
+        </div>
+      );
+    }
 
-	render() {
-		if (this.state.testInfoName) {
-			return (
-				<div className="test-pane">
-					<TestInfoPane 
-						testName={this.state.testInfoName}
-						close={this.close}
-					/>
-				</div>
-			)
-		}
+    let testComponents = this.props.allTests.map((testName, i) => {
+      let className = !this.props.selectedTests.includes(testName)
+        ? 'test'
+        : 'selected test';
+      return (
+        <div className={className}>
+          <span className="testname">
+            <CheckboxLabels
+              label={testName}
+              onClick={() => this.props.onClickTest(testName)}
+              initializeTrue={i === 0}
+            />
+          </span>
+          <span
+            id="info-circle"
+            className="fas fa-info-circle"
+            onClick={() => this.setState({ testInfoName: testName })}
+          ></span>
+        </div>
+      );
+    });
 
-		let testComponents = this.props.allTests.map((testName, i) => {
-			let className = (!this.props.selectedTests.includes(testName)) ? "test" : "selected test";
-			return (
-				<div className={className}>
-					<span className='testname'>
-						<CheckboxLabels label={testName} onClick={() => this.props.onClickTest(testName)} initializeTrue={i===0} />
-					</span>
-					<span id = "info-circle" className="fas fa-info-circle" onClick={() => this.setState({testInfoName: testName})}></span>
-				</div>
-			)
-		})
-
-		return (
-			<div className="test-pane">
-				{/*<div className="message">Click on a test below to toggle it on the graph. Click on the info icons to learn more about each test (what it means, how the data was collected, etc.)</div>*/}
-				{testComponents}
-			</div>
-		)
-	}
+    return <div className="test-pane">{testComponents}</div>;
+  }
 }
 
-export default TestPane
+export default TestPane;
